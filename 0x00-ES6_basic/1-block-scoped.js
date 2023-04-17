1d6287a2b7a1c03efe0d4802847555b6c6a14b94
theopen4 <kielze86@gmail.com>
2023-04-17 12:11:45 +0000

    task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-     task = true;
-     task2 = false;
+    var task = true;
+    var task2 = false;
   }
 
   return [task, task2];
